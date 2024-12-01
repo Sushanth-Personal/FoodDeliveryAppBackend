6acad19959a5ad72670f1c1200096c153db6cd2a
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-01 13:48:26 +0530

    Add: Added media queries of KnowAboutUs componet

--- FoodDeliveryApp/frontend/src/pages/HomePage/components/KnowAboutUs/KnowAboutUs.jsx
+++ FoodDeliveryApp/frontend/src/pages/HomePage/components/KnowAboutUs/KnowAboutUs.jsx
@@ -34,14 +34,14 @@ const KnowAboutUs = () => {
               <p>Place order through our website or Mobile app</p>
             </div>
             <div className = {styles.card}>
-              <h1>Place an Order!</h1>
-              <img src="/orderfood.png" alt="orderfood" />
-              <p>Place order through our website or Mobile app</p>
+              <h1>Track Progress</h1>
+              <img src="/trackfood.png" alt="trackfood" />
+              <p>Your can track your orderstatus with delivery time</p>
             </div>
             <div className = {styles.card}>
-              <h1>Place an Order!</h1>
-              <img src="/orderfood.png" alt="orderfood" />
-              <p>Place order through our website or Mobile app</p>
+              <h1>Get your Order!</h1>
+              <img src="/getorder.png" alt="getorder" />
+              <p>Receive your order at alighting fast speed!</p>
             </div>
           </div>
           <span>Order.UK simplifies the food ordering process. Browse through our diverse menu, select your favorite dishes, and proceed to checkout. Your delicious meal will be on its way to your doorstep in no time!</span>
