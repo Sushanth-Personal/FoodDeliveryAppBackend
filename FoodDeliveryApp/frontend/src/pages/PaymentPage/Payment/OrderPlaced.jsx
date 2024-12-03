962e77b3053544fe102bda9fa414fe4cbe974299
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-03 21:21:12 +0530

    Add; Added Image to Order Placed

--- FoodDeliveryApp/frontend/src/pages/PaymentPage/Payment/OrderPlaced.jsx
+++ FoodDeliveryApp/frontend/src/pages/PaymentPage/Payment/OrderPlaced.jsx
@@ -1,12 +1,16 @@
 import styles from "./orderplaced.module.css";
 import useCart from "../../../customHook/useCart";
 import { useEffect } from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import useImage from "../../../customHook/useImage";
 
 const OrderPlaced = () => {
   const navigate = useNavigate();
   const { cartData: cartItems, loading, error } = useCart();
-
+  const image = useImage(
+    "id",
+    "orderplaced-maincontent-content-1"
+  ).imageURL;
   useEffect(() => {
     console.log("cartItems", cartItems);
   }, [cartItems]);
@@ -17,16 +21,18 @@ const OrderPlaced = () => {
   return (
     <section className={styles.orderPlaced}>
       <div className={styles.mainContent}>
-        <img src="/Content.png" alt="content" />
-        <div className = {styles.orderListContent}>
+        <img
+          id="orderplaced-maincontent-content-1"
+          src={image}
+          alt="content"
+        />
+        <div className={styles.orderListContent}>
           <div className={styles.orderList}>
             <ul>
               {cartItems.map((item) => (
                 <li key={item._id}>
                   <div className={styles.cartItem}>
-                    <p>
-                      {item.productName}
-                    </p>
+                    <p>{item.productName}</p>
                   </div>
                 </li>
               ))}
