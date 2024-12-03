20e266513b118f7aabd24f8b29fe70c05e4152db
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-04 00:15:43 +0530

    add: modify vite

--- FoodDeliveryApp/frontend/vite.config.js
+++ FoodDeliveryApp/frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Explicitly setting the build output directory (default is 'dist')
-  },
   server: {
-    historyApiFallback: true, // Enables the React Router to handle client-side routing
-  }
+    port: process.env.PORT || 4173, // Bind to the Render-provided port, fallback to 4173
+    host: true, // Expose the app to external IPs
+  },
+  build: {
+    outDir: 'dist', // Ensure that build output goes into the 'dist' folder
+  },
 });
