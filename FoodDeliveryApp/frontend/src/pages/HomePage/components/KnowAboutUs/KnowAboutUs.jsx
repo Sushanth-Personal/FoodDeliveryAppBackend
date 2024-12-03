3c2aa58e8fd742c64530fbd6fc35a13a40b87966
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-03 15:48:55 +0530

    Add: Added images of KnowAboutUs component

--- FoodDeliveryApp/frontend/src/pages/HomePage/components/KnowAboutUs/KnowAboutUs.jsx
+++ FoodDeliveryApp/frontend/src/pages/HomePage/components/KnowAboutUs/KnowAboutUs.jsx
@@ -1,6 +1,10 @@
 import styles from "./knowaboutus.module.css";
-
+import useImage from "../../../../customHook/useImage";
+import { displayImage } from "../../../../utility/imageProcess";
 const KnowAboutUs = () => {
+
+const imageURLs = useImage("page", "knowaboutus");
+
   return (
     <div className={styles.knowAboutUs}>
       <header>
@@ -20,31 +24,45 @@ const KnowAboutUs = () => {
           <ul>
             <li>What payment methods are accepted?</li>
             <li>Can I track my order in real-time?</li>
-            <li>
-              Are there any special discounts orpromotions available?
-            </li>
+            <li>Are there any special discounts or promotions available?</li>
             <li>Is Order.UK available in my area?</li>
           </ul>
         </div>
         <div className={styles.rightContent}>
-          <div className = {styles.row1}>
-            <div className = {styles.card}>
+          <div className={styles.row1}>
+            <div className={styles.card}>
               <h1>Place an Order!</h1>
-              <img src="/orderfood.png" alt="orderfood" />
+              <img
+                id="knowaboutus-card-orderfood-1"
+                src={displayImage(imageURLs, "knowaboutus-card-orderfood-1")}
+                alt="orderfood"
+              />
               <p>Place order through our website or Mobile app</p>
             </div>
-            <div className = {styles.card}>
+            <div className={styles.card}>
               <h1>Track Progress</h1>
-              <img src="/trackfood.png" alt="trackfood" />
-              <p>Your can track your orderstatus with delivery time</p>
+              <img
+                id="knowaboutus-card-trackfood-1"
+                src={displayImage(imageURLs, "knowaboutus-card-trackfood-1")}
+                alt="trackfood"
+              />
+              <p>Your can track your order status with delivery time</p>
             </div>
-            <div className = {styles.card}>
+            <div className={styles.card}>
               <h1>Get your Order!</h1>
-              <img src="/getorder.png" alt="getorder" />
-              <p>Receive your order at alighting fast speed!</p>
+              <img
+                id="knowaboutus-card-getorder-1"
+                src={displayImage(imageURLs, "knowaboutus-card-getorder-1")}
+                alt="getorder"
+              />
+              <p>Receive your order at lightning-fast speed!</p>
             </div>
           </div>
-          <span>Order.UK simplifies the food ordering process. Browse through our diverse menu, select your favorite dishes, and proceed to checkout. Your delicious meal will be on its way to your doorstep in no time!</span>
+          <span>
+            Order.UK simplifies the food ordering process. Browse through our
+            diverse menu, select your favorite dishes, and proceed to checkout.
+            Your delicious meal will be on its way to your doorstep in no time!
+          </span>
         </div>
       </div>
     </div>
